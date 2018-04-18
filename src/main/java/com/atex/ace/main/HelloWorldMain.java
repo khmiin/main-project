b2c5840ed1f4014c66deb78e2a52700391d1edbb
hueymiin <huey.miin@atex.com>
2018-04-18 13:38:05 +0800

    first commit

---
+++ src/main/java/com/atex/ace/main/HelloWorldMain.java
@@ -0,0 +1,11 @@
+package com.atex.ace.main;
+
+import com.atex.ace.submodule.ApplicationConfig;
+
+public class HelloWorldMain {
+
+    public static void main(String[] args) {
+        System.out.println(ApplicationConfig.VERSION);
+    }
+
+}
